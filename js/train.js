2f681a1813a307aadfc5bf69dc082dca704e444f
jjaegii <hn06038@naver.com>
2022-07-20 17:41:58 +0900

    .

---
+++ js/train.js
@@ -0,0 +1,22 @@
+let brain;
+
+function setup() {
+  let options = {
+    inputs: 34,
+    outputs: 2,
+    task: "classification",
+    debug: true,
+  };
+  brain = ml5.neuralNetwork(options);
+  brain.loadData("helloNno.json", dataReady);
+}
+
+function dataReady() {
+  brain.normalizeData();
+  brain.train({ epochs: 50 }, finished);
+}
+
+function finished() {
+  console.log("model trained");
+  brain.save();
+}
